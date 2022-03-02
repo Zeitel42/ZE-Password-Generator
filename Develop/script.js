8af5af742cccf3787010cc6a7e5a65c426d7b210
Zeitel42 <43343991+Zeitel42@users.noreply.github.com>
2022-03-02 11:57:29 -0500

    added character types upper and lower case letters, added for loop and  random to generatePassword

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,145 @@
 // Assignment code here
-var randCharArray = [];
-var passwordNum = "";
+var passArray = [];
+var passwordNum = 0;
+var chosenCharacters = [];
 
 // prompt user for password length
 function userPasswordLength() {
-
   var userNum = window.prompt("How many characters would you like your password to be? Please enter a number between 8 and 128.");
-
 // validate length meets criteria
-
-  if(userNum < 8 || userNum > 128){
+  if(userNum < 8 || userNum > 128 || isNaN(userNum)){
     userPasswordLength();
   }else{
-    passwordNum = userNum;
+    var confirmNum = window.confirm("You have chosen to have your password " + userNum + " characters long! Click OK to confirm.");
+    if(confirmNum){
+      passwordNum = parseInt(userNum);
+      bigLetters();
+    }else{
+      userPasswordLength();
+    }
+  }console.log(userNum);
+  console.log(passwordNum);
+}
+
+
+// //  CHARACTER TYPES // //
+
+// PROMPT USER FOR UPPERCASE LETTERS // //
+
+  function bigLetters(){
+    var upperCase = window.prompt("Would you like uppercase letters in your password? Please enter Y or N");
+    
+    if(upperCase === "Y"){
+      var capitals = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      chosenCharacters.push(...capitals);
+      window.alert("Great! Uppercase letters will be part of the password.");
+      littleLetters();
+    }else if(upperCase === "N"){
+      window.alert("Ok. No uppercase letters then.");
+    }else{
+      window.alert("Please enter Y or N to choose.");
+      bigLetters();
+    }console.log(chosenCharacters);
   }
-  console.log(userNum + " local");
-}
-userPasswordLength();
 
-console.log(passwordNum + " extracted");
+  // // PROMPT USER FOR LOWERCASE LETTERS // //
+  function littleLetters() {
+    var lowerCase = window.prompt("Would you like lowercase letters in your password? Please enter Y or N");
 
+    if(lowerCase === "Y"){
+
+      var littles = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      
+      chosenCharacters = chosenCharacters.concat(littles);
+      window.alert("Wonderful! Lowercase letters will be part of the password.");
+      // prompNums();
+    }else if(lowerCase === "N"){
+      window.alert("Ok. No lowercase letters then.");
+
+    }else{
+      window.alert("Please enter Y or N to choose.");
+      littleLetters();
+    }
+  }
+
+// // // PROMPT USER FOR NUMBERS // //
+
+  // function prompNums(){
+  //   var numbers = window.prompt("Would you like numbers in your password?");
+
+  //   if(numbers === "Y"){
+
+  //     var integers = ["0", "1","2", "3", "4", "5", "6", "7", "8", "9"];    
+  //     chosenCharacters = chosenCharacters.concat(integers);
+  //     window.alert("Cool! Numbers letters will be part of the password.");
+      
+  //   }else if(numbers === "N"){
+  //     window.alert("Ok. No numbers letters then.");
+
+  //   }else{
+  //     window.alert("Please enter Y or N to choose.");
+  //     prompNums();
+  //   }
+  // }
+
+// // // PROMPT USER FOR SPECIAL CHARACTERS // //
+
+//   function charSpecial() {
+//     var specials = window.prompt("Would you like special characters in your password?");
+
+//     if(specials === "Y"){
+
+//       var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">"];  
+//       chosenCharacters = chosenCharacters.concat(specialChars);
+//       window.alert("Sweet! Special characters letters will be part of the password.");
+
+//     }else if( specials === "N"){
+//       window.alert("Ok. No special characters letters then.");
+
+//     }else{
+//       window.alert("Please enter Y or N to choose.");
+//       charSpecial();
+//     }
+//}charSpecial();
+
+// console.log(chosenCharacters)
+
+// // GENERATE PASSWORD // //
+
+// generatePassword();
 function generatePassword(){
-  var passwordArray = [];
-  var randomArray = Math.floor(Math.random() * passwordNum);
+  chosenCharacters = [];
+  userPasswordLength();
+  
+  for(var i = 0 ; i < passwordNum; i++){
+    var rando = Math.floor(Math.random()* chosenCharacters.length);
+    passArray.push(...chosenCharacters[rando]);
+  }
 }
+console.log(passwordNum, passArray);
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+//   if(chosenCharacters === []){
+//     window.alert("You must choose at least one character type.");
+//     bigLetters();
+//   }else{
+//     writePassword();
+//   }
+
+// }
+
+
+  
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
